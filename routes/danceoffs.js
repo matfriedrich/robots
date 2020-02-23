b5e86ba0e816886d4ebbbdc9fb25269f9b0afa7b
Matthias Friedrich <matthias.friedrich@student.tugraz.at>
2020-02-23 12:18:22 +0100

    finished /danceoffs endpoint with refined response and ordering

--- routes/danceoffs.js
+++ routes/danceoffs.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var Danceoff = require("../models").Danceoff;
+var Robot = require("../models").Robot;
+var Team = require("../models").Team;
 
 /* GET danceoffs listing. */
 router.get('/', function (req, res, next) {
-    Danceoff.findAll().then((danceoffs) => {
-        res.json(danceoffs);
-    })
+    Danceoff.findAll(
+        {
+            nest: false,
+            raw: true,
+            attributes: ['id'],
+            include: [
+                {
+                    model: Robot, as: 'winner', attributes: ['name', 'id'], include:
+                        {model: Team, attributes: ['name', 'id']}
+                },
+                {
+                    model: Robot, as: 'contestant_one', attributes: ['name']
+                },
+                {
+                    model: Robot, as: 'contestant_two', attributes: ['name']
+                }
+            ]
+            , order: [
+                [{model: Robot, as: 'winner'}, {model: Team}, 'id', 'desc']
+            ]
+        })
+
+        .then((danceoffs) => {
+            res.json(danceoffs);
+        })
 });
 
 
