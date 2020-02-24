ecf7101893b247c8c0c36ed9bf4d10958952b14a
Matthias Friedrich <matthias.friedrich@student.tugraz.at>
2020-02-24 20:26:05 +0100

    authentication

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
+var loginRouter = require('./routes/login');
 var indexRouter = require('./routes/index');
 var robotsRouter = require('./routes/robots');
 var danceoffsRouter = require('./routes/danceoffs');
 
 var app = express();
 
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/robots', robotsRouter);
 app.use('/danceoffs', danceoffsRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,7 +28,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
